30adbf273817204ad03f9615247104d8c9c1630c
tobnys <tobnystrom@gmail.com>
2020-05-25 20:41:09 +0200

    Added dotenv and more structure to the server

--- server.ts
+++ server.ts
@@ -1,11 +1,24 @@
+// General imports
 import express = require("express");
+import { v4 as uuidv4 } from "uuid";
 
+// Environment variables config
+require('dotenv').config()
+
+// General constants
 const app: express.Application = express();
 
-app.get("/", function (req, res) {
+// Global games state variable
+let games = [];
+
+app.get("/v1/game", (req: any, res: any) => {
     res.send("Hello World!");
 });
 
-app.listen(3000, function () {
-    console.log("App is listening on port 3000!");
+app.get("/v1/game/:id", (req: any, res: any) => {
+    console.log("Params", req.params);
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Running server on port: ${process.env.PORT}`);
 });
