52cd6bb711688b22f89a90070ca459f84b351929
tobnys <tobnystrom@gmail.com>
2020-05-26 18:38:42 +0200

    Added proper retrieval of games and error handling for such

--- README.md
+++ README.md
@@ -2,10 +2,26 @@
 
 Simple rock paper scissors backend simulator.
 
+Made using Node & Typescript.
+
 ## How to use
 
+yarn compile to compile the typescript.
+
+yarn start to start the server.
+
+Use Postman or any other API client to test the routes defined below.
 
 ## API definitions
+
+### GET /v1/games
+Retrieves a list of all current (in memory) games.
+
+### GET /v1/games/:id
+Retrieves data about a game by ID.
+
+### POST /v1/game/create
+Creates a game state, storing it in memory and returns a game ID.
 
 ### /v1/games
 Retrieves a list of all current (in memory) games.
--- server.ts
+++ server.ts
@@ -10,25 +10,47 @@ const app: express.Application = express();
 
 // Game type interface
 interface Game {
-    id: string;
+    id: string | null;
     firstMove?: string | null;
     secondMove?: string | null;
     result?: string | null;
 }
 
 // Global games state variable
-let games: Array<Game> = [{id: "1", firstMove: "null", secondMove: "null", result: "null"}];
+let games: Array<Game> = [];
+
+// Helper functions
+function findGameByID(id: string): Game {
+    // Traverse and find the correct game by ID
+    // O(n) - Simple linear traversal
+    for(let i=0; i<games.length; i++) {
+        console.log("GAME TRAVERSE", games[i])
+        if(games[i].id === id) {
+            return games[i];
+        }
+    }
+
+    // Return null if no game was found and handle status in the route handler.
+    return {id: null}
+}
 
 // API handlers 
-
 // List all games in memory
 app.get("/v1/games", (req: any, res: any) => {
     res.json(games);
 });
 
 // List a specific game in memory by ID
-app.get("/v1/game/:id", (req: any, res: any) => {
-    res.json(games);
+app.get("/v1/games/:id", (req: any, res: any) => {
+    // Find the game.
+    let game = findGameByID(req.params.id);
+
+    // Send proper status if the game is not found.
+    if(game.id === null) {
+        res.status(404).send({error: `No game was found using the provided ID: ${req.params.id}`});
+    } else {
+        res.json(game);
+    }
 });
 
 app.post("/v1/game/create", (req: any, res: any) => {
